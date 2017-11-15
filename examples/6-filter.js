023fb5da25e05f07ac2eaa68d1ccd140bf7c9346
kobelb <brandon.kobel@elastic.co>
2017-11-15 10:04:22 -0500

    Using intervals with filter

--- examples/6-filter.js
+++ examples/6-filter.js
@@ -1,6 +1,6 @@
 import { Observable } from 'rxjs/Rx'
 
 export default function () {
-  return Observable.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+  return Observable.interval(1000).take(10)
     .filter(i => i % 2 === 0);
 }
